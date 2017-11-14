b255b62a2f826cae6c2ba961359497ac55d2d5b6
Aaron Heesakkers <aaronheesakkers@gmail.com>
2017-11-14 13:39:05 +0100

    Cleanup

--- lib/rack/bodyparser.rb
+++ lib/rack/bodyparser.rb
@@ -5,7 +5,7 @@ module Rack
     REQUEST_BODY     = 'rack.input'.freeze
     # Where to store in env
     RACK_ENV_KEY     = 'parsed_body'.freeze
-    # Where to store in Rack::Request in case of options[:patch_request]
+    # Where to store in Rack::Request in case of opts[:patch_request]
     RACK_REQUEST_KEY = 'parsed_body'.freeze
 
     # Default error handler
@@ -15,34 +15,57 @@ module Rack
       [400, {}, format(ERROR_MESSAGE, type, e.to_s)]
     end
 
+    HANDLERS = { 'default' => ERROR_HANDLER }.freeze
+
     attr_reader :parsers, :handlers, :logger
 
-    def initialize(app, options = {})
+    def initialize(app, opts = {})
       @app      = app
-      @parsers  = options.delete(:parsers)  || {}
-      @handlers = options.delete(:handlers) || {}
-      @handlers = { 'default' => ERROR_HANDLER }.merge(@handlers)
-      @logger   = options.delete(:logger)
-      @options  = options
+      @parsers  = opts.delete(:parsers) || {}
+      @handlers = HANDLERS.merge(opts.delete(:handlers) || {})
+      @logger   = opts.delete(:logger)
+      @opts     = opts
     end
 
     def call(env)
       type   = Rack::Request.new(env).media_type
       parser = type && detect(parsers, type)
-      body   = parser && env[REQUEST_BODY].read; env[REQUEST_BODY].rewind
-      if body && !body.empty?
-        parser = parser.last
+
+      if parser
         begin
-          parsed = parser.call body
-          env.update RACK_ENV_KEY => parsed
-          patch_rack_request parser, parsed if @options[:patch_request]
+          parse_with(parser.last, env) # parser.last is actual parser
         rescue StandardError => e
-          warn! e, type
-          handler = (detect(handlers, type) || detect(handlers, 'default')).last
-          return handler.call e, type
+          return handle_error(e, type) # return error response
         end
       end
+
+      # return control to app
       @app.call env
+    end
+
+    def request_body(env)
+      body = env[REQUEST_BODY].read
+      env[REQUEST_BODY].rewind
+
+      body
+    end
+
+    def parse_with(parser, env)
+      body = request_body(env)
+      return unless body && !body.empty?
+
+      # parse!
+      parsed = parser.call(body)
+
+      # store results in env, optionally in Rack::Request.
+      env.update(RACK_ENV_KEY => parsed)
+      patch_rack_request(parser, parsed) if @opts[:patch_request]
+    end
+
+    def handle_error(e, type)
+      warn!(e, type)
+      handler = (detect(handlers, type) || detect(handlers, 'default')).last
+      handler.call(e, type)
     end
 
     def patch_rack_request(parser, parsed)
@@ -50,6 +73,7 @@ module Rack
       Rack::Request.send(:define_method, parser.rack_request_key) { parsed }
     end
 
+    # returns [type, parser]
     def detect(hash, what)
       hash.detect do |match, _|
         match.is_a?(Regexp) ? what.match(match) : what.eql?(match)

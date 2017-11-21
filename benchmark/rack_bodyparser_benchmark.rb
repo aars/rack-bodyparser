7d53463cbd538a8881a4593813a2e42ddca14811
Aaron Heesakkers <aaronheesakkers@gmail.com>
2017-11-21 12:51:41 +0100

    Benchmarks

--- benchmark/rack_bodyparser_benchmark.rb
+++ benchmark/rack_bodyparser_benchmark.rb
@@ -9,25 +9,54 @@ def req_opts(content_type = 'plain/text')
   }
 end
 
+# MockParser
+module MockParser
+  def self.env=(env)
+    @env = env
+  end
+
+  def self.call(_body)
+    'parsed body from mock parser'
+  end
+end
+
 Benchmark.ips do |x|
   noop = proc { [200, {}, ['hello']] }
   middleware_opts = {
     parsers: {
-      'plain/text' => proc { 'parsed body' },
-      /regexp/     => proc { 'regexp matched parser' }
+      'plain/text' => proc { 'string matched parser' },
+      /regexp/     => proc { 'regexp matched parser' },
+      'mock'       => MockParser
     }
   }
-  middleware = Rack::BodyParser.new(noop, middleware_opts)
+  middleware_opts_patched = middleware_opts.clone.update(patch_request: true)
+
+  middleware         = Rack::BodyParser.new(noop, middleware_opts)
+  middleware_patched = Rack::BodyParser.new(noop, middleware_opts_patched)
 
   # Compare BodyParser to a simple noop request.
-  parser_req = Rack::MockRequest.new(middleware)
-  noop_req   = Rack::MockRequest.new(noop)
+  noop_req           = Rack::MockRequest.new(noop)
+  parser_req         = Rack::MockRequest.new(middleware)
+  parser_patched_req = Rack::MockRequest.new(middleware_patched)
 
   # And off we go!
-  x.config(time: 10, warmup: 10)
-  x.report('noop')           { noop_req.post('/', req_opts) }
-  x.report('No match')       { parser_req.post('/', req_opts('x')) }
-  x.report('Match (string)') { parser_req.post('/', req_opts) }
-  x.report('Match (regexp)') { parser_req.post('/', req_opts('regexp')) }
+  x.config(time: 5, warmup: 1)
+  x.report('Without BodyParser') { noop_req.post('/', req_opts) } # baseline
+  # Try different setups
+  x.report('No parser found') do
+    parser_req.post('/', req_opts('x'))
+  end
+  x.report('String') do
+    parser_req.post('/', req_opts)
+  end
+  x.report('Regexp') do
+    parser_req.post('/', req_opts('regexp'))
+  end
+  x.report('Str, env') do
+    parser_req.post('/', req_opts('mock'))
+  end
+  x.report('Str, patch_request') do
+    parser_patched_req.post('/', req_opts)
+  end
   x.compare!
 end
